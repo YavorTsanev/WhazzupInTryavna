9900e054df82722c435a1d1548b678c85e0fc38b
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-07-01 15:23:47 +0300

    added privacy policy in privacy.cshtml

--- WhazzupInTryavna.Common/GlobalConstants.cs
+++ WhazzupInTryavna.Common/GlobalConstants.cs
@@ -5,5 +5,9 @@
         public const string SystemName = "Whazz up in Tryavna";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string EmailForContact = "yavor.tsanev.1@gmail.com";
+
+        public const string SiteUrl = "SiteUrl";
     }
 }
