25b46f8ca9804e7e7acda6cdd2d980db69d367ae
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-08-09 16:08:08 +0300

    move WhazzupInTryavna.IntegrationTests in folder Tests

---
+++ Tests/WhazzupInTryavna.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace WhazzupInTryavna.IntegrationTests
+{
+    using System;
+    using Xunit;
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
